95b04c4e0aa40d0863808077adfe3e1ce9610cd2
RukInDaHouse <r1997p@mail.ru>
2020-10-06 18:04:53 +0300

    add timeline script

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,11 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
